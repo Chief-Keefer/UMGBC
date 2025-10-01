6ab9b4a220e815042574525ee1a5ecc1d4f5f46f
Keith Phan <157994660+Chief-Keefer@users.noreply.github.com>
2025-09-30 20:40:29 -0400

    Event Exercise

--- JavaScript/Intermediate/Event-Exercises/script.js
+++ JavaScript/Intermediate/Event-Exercises/script.js
@@ -1,17 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const colorInput = document.getElementById("colorInput");
-  const newColorButton = document.querySelector("button");
+  const colorForm = document.getElementById("color-form");
+  const colorInput = document.getElementById("color-input");
   const newBoxButton = document.getElementById("new-box-button");
-  const boxes = document.querySelectorAll(".box");
-  newColorButton.addEventListener("click", function (e) {
+  const boxContainer = document.getElementById("box-container");
+  let boxColor = null;
+  let boxID = 0;
+
+  function createBox() {
+    const box = document.createElement("div");
+    ++boxID;
+    box.setAttribute("box-id", boxID.toString());
+    box.className = "box";
+    box.style.backgroundColor = boxColor;
+    box.textContent = `Box ${boxID}`;
+
+    return box;
+  }
+
+  colorForm.addEventListener("submit", function (e) {
     e.preventDefault();
-    const color = colorInput.value;
-    boxes.forEach((box) => {
-      box.style.backgroundColor = color;
-    });
+    const newColor = colorInput.value.trim();
+
+    const boxes = document.querySelectorAll(".box");
+    for (const box of boxes) {
+      box.style.backgroundColor = newColor;
+    }
+
+    colorInput.value = "";
+    boxColor = newColor;
   });
 
   newBoxButton.addEventListener("click", function () {
-    // Code to create a new box will go here
+    const box = createBox();
+    boxContainer.appendChild(box);
+  });
+
+  document.addEventListener("dblclick", function (e) {
+    if (e.target.classList.contains("box")) {
+      e.target.remove();
+    }
+  });
+
+  document.addEventListener("mouseover", function (e) {
+    if (e.target.classList.contains("box")) {
+      e.target.textContent = `X: ${e.pageX}, Y: ${e.pageY}`;
+    }
+  });
+
+  document.addEventListener("mouseout", function (e) {
+    if (e.target.classList.contains("box")) {
+      const id = e.target.getAttribute("box-id");
+      e.target.textContent = `Box ${id}`;
+    }
+  });
+
+  window.addEventListener("keydown", function (e) {
+    if (e.target.id === "color-input") return;
+
+    if (e.key === "n" || e.key === "N") {
+      const box = createBox();
+      boxContainer.appendChild(box);
+    }
   });
 });
